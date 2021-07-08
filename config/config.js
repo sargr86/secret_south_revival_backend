c73ff927d14e4d8bc3a31a37898fac5ea6e0e0ef
sargr86 <sargr86@gmail.com>
2021-07-08 17:28:13 +0400

    Config changes

--- config/config.js
+++ config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": process.env.DEVELOPMENT_DB_PASS,
-    "database": "secret_south_new",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+    "development": {
+        "username": "root",
+        "password": process.env.DEVELOPMENT_DB_PASS,
+        "database": "secret_south_new",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "production": {
+        "username": process.env.PRODUCTION_DB_USER,
+        "password": process.env.PRODUCTION_DB_PASS,
+        "database": process.env.PRODUCTION_DB,
+        "host": process.env.PRODUCTION_HOST,
+        "dialect": "mysql"
+    }
 }
